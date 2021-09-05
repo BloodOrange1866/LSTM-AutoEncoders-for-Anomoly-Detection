fadfdcb3485197ea59351656b14e7e3a40a9375b
Cihan1994 <CDogan1994@hotmail.co.uk>
2021-09-05 21:09:20 +0100

    Final update to README and various architecture changes.

--- src/model.py
+++ src/model.py
@@ -21,38 +21,36 @@ class Encoder(nn.Module):
         self.seq_len, self.n_features = seq_len, n_features
         self.embedding_dim, self.hidden_dim = embedding_dim, 2 * embedding_dim
 
-        #self.rnn1 = nn.LSTM(
-        #    input_size=n_features,
-        #    hidden_size=self.hidden_dim,
-        #    num_layers=1,
-        #    batch_first=True,
-        #)
+        self.rnn1 = nn.LSTM(
+            input_size=n_features,
+            hidden_size=self.hidden_dim,
+            num_layers=1,
+            batch_first=True,
+        )
 
-        # Not used too complex
-        #self.rnn2 = nn.LSTM(
-        #    input_size=self.hidden_dim,
-        #    hidden_size=embedding_dim,
-        #    num_layers=1,
-        #    batch_first=True
-        #)
+        self.rnn2 = nn.LSTM(
+            input_size=self.hidden_dim,
+            hidden_size=embedding_dim,
+            num_layers=1,
+            batch_first=True
+        )
 
         #self.dropout = nn.Dropout(0.15)
 
-        self.rnn1 = nn.LSTM(
-            input_size=self.n_features,
-            hidden_size=embedding_dim,
-            num_layers=1,
-            batch_first=True,
-            dropout=0.15,
-        )
+        #self.rnn1 = nn.LSTM(
+        #    input_size=self.n_features,
+        #    hidden_size=embedding_dim,
+        #    num_layers=2,
+        #    batch_first=True,
+        #    dropout=0.15,
+        #)
 
     def forward(self, x):
         x = x.reshape((1, self.seq_len, self.n_features))
-        #x, (_, _) = self.rnn1(x)
+        x, (_, _) = self.rnn1(x)
         #x = self.dropout(x)
-        #x, (hidden_n, _) = self.rnn2(x)
+        x, (hidden_n, _) = self.rnn2(x)
 
-        x, (hidden_n, _) = self.rnn1(x)
         return hidden_n.reshape((self.n_features, self.embedding_dim))
 
 
@@ -62,30 +60,30 @@ class Decoder(nn.Module):
         self.seq_len, self.input_dim = seq_len, input_dim
         self.hidden_dim, self.n_features = 2 * input_dim, n_features
 
-        #self.rnn1 = nn.LSTM(
-        #    input_size=input_dim,
-        #    hidden_size=input_dim,
-        #    num_layers=1,
-        #    batch_first=True,
-        #)
+        self.rnn1 = nn.LSTM(
+            input_size=input_dim,
+            hidden_size=input_dim,
+            num_layers=1,
+            batch_first=True,
+        )
 
-        # Not used too complex
-        #self.rnn2 = nn.LSTM(
-        #    input_size=input_dim,
-        #    hidden_size=self.hidden_dim,
-        #    num_layers=1,
-        #    batch_first=True
-        #)
+
+        self.rnn2 = nn.LSTM(
+            input_size=input_dim,
+            hidden_size=self.hidden_dim,
+            num_layers=1,
+            batch_first=True
+        )
 
         #self.dropout = nn.Dropout(0.15)
 
-        self.rnn1 = nn.LSTM(
-            input_size=input_dim,
-            hidden_size=self.hidden_dim,
-            num_layers=2,
-            batch_first=True,
-            dropout=0.15
-        )
+        #self.rnn1 = nn.LSTM(
+        #    input_size=input_dim,
+        #    hidden_size=self.hidden_dim,
+        #    num_layers=2,
+        #    batch_first=True,
+        #    dropout=0.15
+        #)
 
         self.output_layer = nn.Linear(self.hidden_dim, n_features)
 
@@ -94,6 +92,6 @@ class Decoder(nn.Module):
         x = x.reshape((self.n_features, self.seq_len, self.input_dim))
         x, (_, _) = self.rnn1(x)
         #x = self.dropout(x)
-        #x, (_, _) = self.rnn2(x)
+        x, (_, _) = self.rnn2(x)
         x = x.reshape((self.seq_len, self.hidden_dim))
         return self.output_layer(x)
